325e182988c5395e84de3d7c0df90774606fb0fb
jainmani26 <93339993+jainmani26@users.noreply.github.com>
2021-12-12 12:53:03 +0530

    Create Broken Economy2.java

---
+++ Broken Economy2.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static int floor(int []arr, int d)
+    {
+        int left = 0, right = arr.length - 1;
+     
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] >= d)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return arr[right];
+    }
+    
+    public static int ceil(int []arr, int d)
+    {
+        int left = 0, right = arr.length - 1;
+     
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(arr[mid] <= d)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return arr[left];
+    }
+    
+
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner scn = new Scanner (System.in);
+        int n = scn.nextInt();
+        int []arr = new int [n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+        int d = scn.nextInt();
+        System.out.println(ceil(arr,d));
+        System.out.println(floor(arr,d));
+        
+        
+    }
+
+}
